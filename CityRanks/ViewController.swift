6fcdc2656c3c2776765491887adba7531de7a092
Piotr Łyczba <lyzkov@gmail.com>
2020-08-05 13:25:42 +0200

    Initial Commit

---
+++ CityRanks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityRanks
+//
+//  Created by lyzkov on 05/08/2020.
+//  Copyright © 2020 lyzkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
